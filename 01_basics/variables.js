49323c22573e695ca032e5175682e4c78f68f6af
Sadik Nazrul <108599764+sadik-nazrul@users.noreply.github.com>
2024-01-04 03:51:36 +0000

    Variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "sadik@google.com"
+var accountPassword = "123456"
+accountCity = "Sherpur"
+
+
+// accountId = 122552 not allowed
+
+accountEmail = "sd@sd.com"
+accountPassword = "3e43523"
+accountCity = "jamalpur"
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+Var use not suggested
+have issue with block scope and functional scope
+*/
