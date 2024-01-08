0c81dc07efe28a802f682bdd129fed8f63341a52
Sadik Nazrul <108599764+sadik-nazrul@users.noreply.github.com>
2024-01-08 05:20:49 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,3 +1,23 @@
 "use strict"; // treat all code as newer version
 
 //alert(3+3) //we are using node not browser
+
+
+let name = "Sadik"
+let age = "26"
+let isLogedIn = false
+let state;
+
+
+
+// number => 2 to power 53
+// string => ""
+// boolean => true/false
+// null => it's a standalone value. is em
+// undifined => value not assigned
+// Symbol => Use for talk unique
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
