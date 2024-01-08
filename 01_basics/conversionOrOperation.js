6c1f6795f38e92a55067974a5449c972d89406f0
Sadik Nazrul <108599764+sadik-nazrul@users.noreply.github.com>
2024-01-08 08:49:39 +0000

    Conversion

---
+++ 01_basics/conversionOrOperation.js
@@ -0,0 +1,58 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// ================= Conversion ====================
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+/*
+1. If we are trying to convert a string like 33 to a number it's give me result 33 type is showing number. 
+
+2. If we are trying to convert string 33abc to a number it's give me result NaNtype is showing number.
+
+3. true => 1/ false => 1
+
+4. "sadik" => NaN
+
+5. undefided => NaN
+
+6. null => 0
+*/
+
+// ================= Conversion ====================
+
+let isLogedIn = 1
+
+let booleanIsLogedIn = Boolean(isLogedIn)
+
+// console.log(booleanIsLogedIn);
+// console.log(typeof(booleanIsLogedIn));
+
+// 1 => true; 0 => false
+// "sadik" => true
+
+
+// ================= Conversion ====================
+
+let someNumber = false
+
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+// 33 => 33(string)
+// true/false => true/false(string)
+
+// ================= Operation ====================
