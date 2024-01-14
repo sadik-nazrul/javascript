aea0da5af52930b787d329ac7ac28ccc6967d23a
Sadik Nazrul <108599764+sadik-nazrul@users.noreply.github.com>
2024-01-14 02:06:45 +0000

    srting

---
+++ 01_basics/string.js
@@ -0,0 +1,26 @@
+const name = "Sadik"
+// console.log(name);
+
+const myName = new String("Sadik")
+// console.log(myName);
+
+// ********* Methods *********
+
+// console.log(myName.length);
+/* 
+console.log(myName.charAt(2));
+Similers
+console.log(`My name character ${myName.charCodeAt(3)} is equal to ${myName.charAt(3)}`);
+console.log(myName.codePointAt(3));
+console.log(myName.at(3)); 
+*/
+
+const firstName = "Sadik"
+const lastName = "Nazrul"
+
+const anotherString = "I love my Country"
+
+// console.log(firstName.concat(',', ' ', lastName));
+// console.log(anotherString.endsWith('Country'));
+// console.log(anotherString.includes("d"));
+console.log(anotherString.indexOf('my'));
