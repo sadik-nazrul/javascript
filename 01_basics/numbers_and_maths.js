2cd04e1eea1ce1f4dcceadb73b3a56abee56d718
Yoursadik-nazrul <sadikull3@gmail.com>
2024-02-04 07:21:53 +0600

    number

---
+++ 01_basics/numbers_and_maths.js
@@ -0,0 +1,11 @@
+const roll = 102;
+// console.log(roll);
+
+const serial = new Number(1030);
+// console.log(serial);
+
+// console.log(serial.toString().length);
+// console.log(serial.toFixed(1));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-BD'));
