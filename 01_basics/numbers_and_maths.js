c928b8dc401b5be2800be76617ebcc4d5e944c83
Yoursadik-nazrul <sadikull3@gmail.com>
2024-02-15 08:49:24 +0600

    Numbers

--- 01_basics/numbers_and_maths.js
+++ 01_basics/numbers_and_maths.js
@@ -1,3 +1,52 @@
+// Javascript e number decimal ebong decimal sara lekha jay
+let a = 123
+let b = 13.00
+// console.log(a,b);
+
+
+// Proyojone amra boro number evabe lekhte pari
+let c = 145e6 //er result ashbe 145000000
+let d = 159e-4 //er result ashbe 0.0159
+// console.log(c);
+// console.log(d);
+
+
+// Javascript e number 15 porjonto thik moto result show kore er beshi dile jhamela kore fele decimal hole 17 er upore gele genjam kore fele
+let e = 999999999999999 //er Result 999999999999999
+let f = 111111111111111111 //er Result 111111111111111100 - genjam kore felche
+// console.log(e);
+// console.log(f);
+
+
+// JS e floating number er arithmetic operation shob shomoy 100% accurate hoyna
+let g = 0.2 + 0.1
+// console.log(g); // er Result howar kotha 0.3 kintu ashche 0.30000000000000004
+// ei problem solve korar upay holo
+let h = (0.2 * 10 + 0.1 * 10) / 10
+// console.log(h);
+
+
+// Number er shatrhe number addition korle number result ashbe kintu string er shathe number addition korle string ashe.
+let i = 22
+let j = 43
+let add = i+j
+// console.log(typeof(add));
+let k = "42"
+let l = 42
+// console.log(k+l);
+
+
+// Number er shathe string er onno arithmatic operation korle ba decimal string er shathe decimal string onno operation korle number result number ashbe ebong hishab hoye ashbe. real string er shathe number er onno arithmatic operation korle result NaN ashbe
+let x = 10;
+let y = "10";
+let z = x * y;
+// console.log(z);
+// console.log(typeof(z));
+let m = "Sadik"
+let n = 10
+let o = m/n
+console.log(o);
+
 const roll = 102;
 // console.log(roll);
 
@@ -8,4 +57,4 @@ const serial = new Number(1030);
 // console.log(serial.toFixed(1));
 
 const hundreds = 10000000
-console.log(hundreds.toLocaleString('en-BD'));
+// console.log(hundreds.toLocaleString('en-BD'));
